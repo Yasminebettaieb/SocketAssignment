eeccb168cbb9678297270cfedf305dbab4154ced
Yasminebettaieb <71673006+Yasminebettaieb@users.noreply.github.com>
2021-12-06 17:57:34 +0000

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,16 @@
-
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+
     static final int port = 1200;
 
     public static void main(String[] args) throws Exception {
 
         // Listen to a specific port
 
-        ServerSocket serverSocket = new ServerSocket(1200);
+        ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Waiting for connection");
         Socket socClient = serverSocket.accept(); // Accept a client socket
         System.out.println("Connection established");
@@ -19,8 +18,13 @@ public class Server {
         // Initialize in / out
         BufferedReader inServer = new BufferedReader(new InputStreamReader(socClient.getInputStream()));
         PrintWriter outServer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socClient.getOutputStream())), true);
+        String msg = inServer.readLine();
 
-        System.out.println(inServer.lines());
+        System.out.println(msg);
+        String string = "";
+        System.out.println("Removing vowels...");
+        string = msg.replaceAll("[aeiouAEIOU]", "");
+        outServer.println("OUTPUT:"+ string);
 
         // Close in / out
         inServer.close();
